5971c80dd5f02ad8f2a1b1157535382f02454b2e
Ernest-bio <mrbegican@gmail.com>
2025-10-31 11:42:53 +0300

    Add files via upload

--- experiments/api_example/api_reader.py
+++ experiments/api_example/api_reader.py
@@ -1,1 +1,15 @@
+import requests
+import pandas as pd
 
+url = "https://rickandmortyapi.com/api/character"
+
+response = requests.get(url)
+data = response.json() 
+
+characters = data["results"]
+
+df = pd.DataFrame(characters)
+
+print(df.head(10))
+
+df.to_csv("rick_and_morty_characters.csv", index=False)
